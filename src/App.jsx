97cf8e3ab3d16e65909f9c3fa98bb73164bbb6ac
Wayne Stedman <waynestedman@users.noreply.github.com>
2022-12-07 18:19:34 -0800

    added css reset

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Motion in React</h1>
     </div>
   )
 }
