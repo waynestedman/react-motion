725ffb295274b28d36b197e4382fdd8e213a4bf3
Wayne Stedman <waynestedman@users.noreply.github.com>
2022-12-12 15:59:12 -0800

    build: button #2

--- src/CssMotion.jsx
+++ src/CssMotion.jsx
@@ -27,7 +27,21 @@ function CssMotion() {
         </div>
         <div className="menuTrigger">
           <h2>Menu trigger example #2</h2>
-          <button className="button2" aria-controls="primary-navigation" aria-expanded="false"></button>
+          <button className="button2" aria-controls="primary-navigation" aria-expanded="false">
+            <svg className="hamburger" viewBox="0 0 100 100" width="200">
+              <line className="line top"
+              x1="90" x2="10" y1="40" y2="40"
+              strokeWidth="10" strokeLinecap="round" strokeDasharray="80" strokeDashoffset="0"
+              >
+              </line>
+              <line className="line bottom"
+              x1="10" x2="90" y1="60" y2="60"
+              strokeWidth="10" strokeLinecap="round" strokeDasharray="80" strokeDashoffset="0"
+              >
+              </line>
+
+            </svg>
+          </button>
         </div>
         <div className="menuTrigger">
           <h2>Menu trigger example #3</h2>
